765a3f748286a6846aa86466654ae5cd1af2b1f8
Adekunle Oshodi <kunleoshodi856@gmail.com>
2023-04-29 19:13:44 -0500

    added if statement for allItems to increment to results object

---
+++ countOnly.js
@@ -0,0 +1,64 @@
+const assertEqual = function (actual, expected) {
+  const isAssertionPassed = " Assertion Passed: ";
+  const isAssertionFailed = " Assertion Failed: ";
+
+  if (actual === expected) {
+    console.log(
+      String.fromCodePoint(0x2705) +
+        String.fromCodePoint(0x2705) +
+        String.fromCodePoint(0x2705) +
+        `${isAssertionPassed}` +
+        actual +
+        " === " +
+        expected
+    );
+  } else {
+    console.log(
+      String.fromCodePoint(0x274c) +
+        String.fromCodePoint(0x274c) +
+        String.fromCodePoint(0x274c) +
+        `${isAssertionFailed}` +
+        actual +
+        " !== " +
+        expected
+    );
+  }
+};
+
+const countOnly = function (allItems, itemsToCount) {
+  const results = {};
+
+  for (const item of allItems) {
+    if (results[item]) {
+      results[item] += 1;
+    } else {
+      results[item] = 1;
+    }
+  }
+
+  return results;
+};
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe",
+];
+
+const result1 = countOnly(firstNames, {
+  Jason: true,
+  Karima: true,
+  Fang: true,
+  Agouhanna: false,
+});
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
