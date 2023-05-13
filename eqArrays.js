bf6ac875d530cee506e5ebb5fc08df2703e6e1ee
Adekunle Oshodi <kunleoshodi856@gmail.com>
2023-05-13 17:58:23 -0500

    refactored eqArrays.js

--- eqArrays.js
+++ eqArrays.js
@@ -1,30 +1,3 @@
-const assertEqual = function (actual, expected) {
-  const isAssertionPassed = " Assertion Passed: ";
-  const isAssertionFailed = " Assertion Failed: ";
-
-  if (actual === expected) {
-    console.log(
-      String.fromCodePoint(0x2705) +
-        String.fromCodePoint(0x2705) +
-        String.fromCodePoint(0x2705) +
-        `${isAssertionPassed}` +
-        actual +
-        " === " +
-        expected
-    );
-  } else {
-    console.log(
-      String.fromCodePoint(0x274c) +
-        String.fromCodePoint(0x274c) +
-        String.fromCodePoint(0x274c) +
-        `${isAssertionFailed}` +
-        actual +
-        " !== " +
-        expected
-    );
-  }
-};
-
 const eqArrays = function (arrayNum1, arrayNum2) {
   if (arrayNum1.length !== arrayNum2.length) {
     return false;
@@ -39,13 +12,10 @@ const eqArrays = function (arrayNum1, arrayNum2) {
   return true;
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true);
-
 eqArrays([1, 2, 3], [1, 2, 3]);
 eqArrays([1, 2, 3], [3, 2, 1]);
 
 eqArrays(["1", "2", "3"], ["1", "2", "3"]);
 eqArrays(["1", "2", "3"], ["1", "2", 3]);
+
+module.exports = eqArrays;
