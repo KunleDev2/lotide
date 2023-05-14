e33fcae2ccd56cefe06dfcbf86a872d17ccf751c
Adekunle Oshodi <kunleoshodi856@gmail.com>
2023-05-13 19:20:53 -0500

    refactored tailTest.js and middleTest.js

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,16 @@
-const assertEqual = require("../assertEqual.js");
 const tail = require("../tail.js");
+const assert = require('chai').assert;
 
-assertEqual(tail(["Lighthouse Labs"]).length, 0);
-assertEqual(tail([1, 2, 3, 4, 5, 6, 7]).length, 6);
-assertEqual(tail([]).length, 0);
+describe("#tail", () => {
+  it("returns 0 for ['Lighthouse Labs'] array length", () => {
+    assert.strictEqual(tail(["Lighthouse Labs"]).length, 0);
+  });
+
+  it("returns 6 for [1, 2, 3, 4, 5, 6, 7] array length", () => {
+    assert.strictEqual(tail([1, 2, 3, 4, 5, 6, 7]).length, 6);
+  });
+
+  it("returns 0 for [] array length", () => {
+    assert.strictEqual(tail([]).length, 0);
+  });
+});
